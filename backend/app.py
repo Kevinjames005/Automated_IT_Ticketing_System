9348b59fe79d7506464b32bbaaa0948af08a7664
Kevin James <jesmjamind11@gmail.com>
2026-02-09 20:00:30 +0530

    added API-KEY for backend security

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,22 @@
+import os
 from flask import Flask,request,jsonify
 from ml.inference import predict
 
 
 app = Flask(__name__)
 
+API_KEY = os.getenv("API_KEY")
+if not API_KEY:
+    raise RuntimeError("API_KEY not set in environment")
+
 @app.route("/classify",methods=["POST"])
 
 def classify():
+    client_key = request.headers.get("X-API-KEY")
+
+    if client_key != API_KEY:
+        return jsonify({"error": "Unauthorized"}), 401
+
     data = request.get_json()
     if not data or "subject" not in data or "body" not in data:
         return {"error": "Invalid input"}, 400
